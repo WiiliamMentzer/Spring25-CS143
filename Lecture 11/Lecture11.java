f7c01c63909fd2b2af662f87eaa3216acb6eff38
DESKTOP-1RIPGT3\wiiDev <william.mentzer2k@gmail.com>
2025-05-13 11:56:17 -0700

    Commit lec 11 and 12, still WIP.

--- Lecture 11/Lecture11.java
+++ Lecture 11/Lecture11.java
@@ -16,19 +16,50 @@ class Lecture11 {
         1. {29, 17, 3, 94} {46, 8 -4, 12} - init split in half
         2. {29, 17}{3, 94} {46, 8}{-4,12} - recursion split in half
         3. {29}{17}{3}{94} {46}{8}{-4}{12} - index length 1 achived, now beign sorting check val 1 vs val 2 and store less first in new array
+            17 vs 29, add 17, last index compareator reached add 29
+            3 vs 94, add 3, last index comparator reached add 94
+            (SWAP SIDES)
+            46 vs 8, add 8, last index comparator reached add 46
+            -4 vs 12, add -4, last index comparator reached add 12
         4. {17, 29}{3, 94} {8, 46}{-4, 12} - resursively sort arrays checking
+            3 vs 17 -> add 3 first, index arr2 moves forward
+            17 vs 94 -> add 17 first, index arr1 moves forward
+            29 vs 94 -> add 29 first, index arr1 cannot move forward, Length reached
+            94 vs . -> add 94, index arr2 cannot move forward, Length reached
+            (SWAP SIDES)
+            8 vs -4 -> add -4 first, index arr2 moves forward
+            8 vs 12 -> add 8 first, index arr1 moves forward
+            46 vs 12 -> add 12 first, index arr2 cannot move forward, length reached
+            46 vs . -> add 46, index arr1 cannot move forward, Length reached
         5. {3, 17, 29, 94} {-4, 8, 12, 46}
-        6. {-4, 3, 8, 12, 17, 29, 46, 94}
+            3 vs -4 -> add -4 first, index arr2 moves forward
+            3 vs 8 -> add 3 first, index arr1 moves forward
+            17 vs 8 -> add 8 first, index arr2 moves forward
+            29 vs 12 -> add 12 first, index arr2 moves forward
+            29 vs 46 -> add 29 first, index arr1 moves forward
+            94 vs 46 -> add 46 first, index arr2 cannot move forward, length reached
+            96 vs . -> add 96, index arr1 cannot move forward, Length reached
+        6. FIN: {-4, 3, 8, 12, 17, 29, 46, 94}
         */
 
         // B. {6, 5, 3, 7, 1, 8, 4, 2}
         /*
-
+            1. {6, 5, 3, 7} {1, 8, 4, 2} - init split in half
+            2. {6, 5}{3, 7} {1, 8}{4, 2} - recursion split in half
+            3. {6}{5}{3}{7} {1}{8}{4}{2} - index length 1 achived, now beign sorting check val 1 vs val 2 and store less first in new array
+            4. {5, 6}{3, 7} {1, 8}{2, 4}
+            5. {3, 5, 6, 7} {1, 2, 4, 8}
+            6. FIN: {1, 2, 3, 5, 5, 6, 7, 8}
         */
 
         // C. {33, 14, 3, 95, 47, 9, –42, 13}
         /*
-
+            1. {33, 14, 3, 95} {47, 9, -42, 13} - init split in half
+            2. {33, 14}{3, 95} {47, 9}{-42, 13} - recursion split in half
+            3. {33}{14}{3}{95} {47}{9}{-42}{13} - index length 1 achived, now beign sorting check val 1 vs val 2 and store less first in new array
+            4. {14, 33}{3, 95} {9, 47}{-42, 13}
+            5. {3, 14, 33, 95} {-42, 9, 13, 47}
+            6. FIN: {-42,3, 9, 13, 14, 33, 47, 95}         
         */
 
         // Exercises:
@@ -42,7 +73,24 @@ class Lecture11 {
 
 
         // A. int[] numbers3 = {8, 5, –9, 14, 0, –1, –7, 3};
+        /*
+            1. {8, 5, -9, 14} {0, -1, -7, 3} - init split in half
+            2. {8, 5}{-9, 14} {0, -1}{-7, 3} - recursion split in half
+            3. {8}{5}{-9}{14} {0}{-1}{-7}{3} - index length 1 achived, now beign sorting check val 1 vs val 2 and store less first in new array
+            4. {5, 8}{-9, 14} {-1, 0}{-7, 3}
+            5. {-9, 5, 8, 14} {-7, -1, 0, 3}
+            6. FIN: {-9, -7, -1, 0, 3, 5, 8, 14}         
+        */
+
         // B. int[] numbers4 = {15, 56, 24, 5, 39, –4, 27, 10};
+        /*
+            1. {15, 56, 24, 5} {39, -4, 27, 10} - init split in half
+            2. {15, 56}{24, 5} {39, -4}{27, 10} - recursion split in half
+            3. {15}{56}{24}{5} {39}{-4}{27}{10} - index length 1 achived, now beign sorting check val 1 vs val 2 and store less first in new array
+            4. {15, 56}{5, 24} {-4,39}{10, 27}
+            5. {5, 15, 24, 56} {-4, 10, 27, 39}
+            6. FIN: {-4, 5, 1-, 15, 24, 27, 39, 56}         
+        */
 
         // 13. Using the following arrays, trace the complete execution of the merge
         // sort algorithm when called on each array.
@@ -54,14 +102,33 @@ class Lecture11 {
         mergeSort(numbers5);
         System.out.println(Arrays.toString(numbers5));
 
+        /*
+            1. {22, 44, 11, 88} {66, 33, 55, 77} - init split in half
+            2. {22, 44}{11, 88} {66, 33}{55, 77} - recursion split in half
+            3. {22}{44}{11}{88} {66}{33}{55}{77} - index length 1 achived, now beign sorting check val 1 vs val 2 and store less first in new array
+            4. {22, 44}{11, 88} {33, 66}{55, 77}
+            5. {11, 22, 44, 88} {33, 55, 66, 77}
+            6. FIN: {11, 22, 33, 44, 55, 66, 77, 88}         
+        */
+
 
 
         // B. int[] numbers6 = {–3, -6, -1, -5, 0, -2, -4, -7};
         int[] numbers6 = {-3, -6, -1, -5, 0, -2, -4, -7};
         mergeSort(numbers6);
         System.out.println(Arrays.toString(numbers6));
+        /*
+            1. {-3, -6, -1, -5} {0, -2, -4, -7} - init split in half
+            2. {-3, -6}{-1, -5} {0, -2}{-4, -7} - recursion split in half
+            3. {-3}{-6}{-1}{-5} {0}{-2}{-4}{-7} - index length 1 achived, now beign sorting check val 1 vs val 2 and store less first in new array
+            4. {-6, -3}{-5, -1} {-2, 0}{-7, -4}
+            5. {-6, -5, -3, -1} {-7, -4, -2, 0}
+            6. FIN: {-7, -6, -5, -4, -3, -2, -1, 0}         
+        */
     }
 
+
+    //Added Merge and MergeSort to help visualize code in IDE, removed 
     public static void merge (int[] result, int[] arr1, int[] arr2) {
         int i1 = 0; // index into left array
         int i2 = 0; // index into right array
--- Lecture 12/Lecture12.java
+++ Lecture 12/Lecture12.java
@@ -1,38 +1,52 @@
+import java.util.*;
 class Lecture12 {
     public static void main(String[] args) {
         System.out.println("Hello from lecture 12");
-
+        /*
         // Self-Check Problems:
         // Section 14.1: Stack/Queue Basics
 
         // 1. Which of the following statements about stacks and queues is true?
-
+        
         // A. Stacks and queues can store only integers as their data.
+           --FALSE, Stacks and queues can handle any object you put in, as long as you do something with it
         // B. A stack returns elements in the same order as they were added (first-in,
         // first-out).
-        // C. A queue’s remove method removes and returns the element at the front of
-        // the
-        // queue.
+           --FALSE, queue is FIFO
+        // C. A queue’s remove method removes and returns the element at the front of the queue.
+            --True, remove does remove the front value and returns it at the front of the queue
         // D. Stacks and queues are similar to lists, but less efficient.
+            --False, Stacks and Queues use similar methods to lists, and not only that, they are efficient ass well at it too beign their own collection
         // E. The peek method allows access to the element at the bottom of a stack.
-
+            --FALSE, looks at the TOP of the stack instead of the bottom.
         // 2. What is a real-world example of data that could be modeled using a stack?
         // Using a queue?
+            --Stack: Picking up a tray in a dining hall, first in first out.
+            --Queue: Elevator / line at disneyland where you are put into a QUEUE
 
         // 3. When you call push on a stack, where is the new element placed relative to
         // the other elements in the stack?
+            --Straight to the top of the stack
         // When you call pop, which element from the stack is returned?
+            --The top most element from the stack is returned
 
         // 4. When you call add on a queue, where is the new element placed relative to
         // the other elements in the queue?
+            --To the end of the queue it goes when using add
         // When you call remove, which element from the queue is returned?
+            --Remove will take the front element and remove it from the queue after returning the value.
 
         // 5. If you create a new empty stack and push the values 1, 2, and 3 in that
+            -- (3, 2, 1)
         // order, and call pop on the stack once, what value will be returned?
+            (2, 1)
 
         // 6. If you create a new empty queue and add the values 1, 2, and 3 in that
         // order, and call remove on the queue once, what value will be returned?
-
+            --(1, 2, 3)
+            --remove 
+            --(2, 3)
+        
         // Section 14.2: Common Stack/Queue Operations
 
         // 12. Stacks and queues have less functionality than other similar collections
@@ -40,11 +54,59 @@ class Lecture12 {
         // Why are they still useful despite lacking functionality? What possible
         // advantages are there of using a less powerful collection?
 
+        --They are increadibly efficient at what they do. Not only that if we want the objects in question in a certain order another collection would need more work to have the functionality of a queue/ stack
+
         // 13. What is the output of the following code?
         // Do this by hand to make sure that you understand stacks and queues
+        
+        Stack<String> s = new Stack<>();
+        Queue<String> q = new LinkedList<>();
+        s.push("how");
+        s.push("are");
+        s.push("you");
+        while (!s.isEmpty()) {
+            q.add(s.pop());
+        }
+        System.out.println(q);
+        
+
+        1. We start by creating a stack and a queue.
+        2. We add "how" "are" "you" to the stack resulting in ("you", "are", "how")
+        3. while the stack isnt empty , add to the queue by popping the top value and adding it to the queue, first "you", then "are", last "how"
+        4. Queue now looks like ("you", "are", "how"), and stack is empty from pop
+        5. we print out the queue ("you", "are", "how")
 
         // 14. What is the output of the following code?
         // Do this by hand to make sure that you understand stacks and queues
+        */
+        Stack s = new Stack<>();
+        s.push(7);
+        s.push(10);
+        System.out.println(s.pop());
+        System.out.println(s.peek());
+        s.push(3);
+        s.push(5);
+        System.out.println(s.pop());
+        System.out.println(s.isEmpty());
+        System.out.println(s.size());
+        System.out.println(s.peek());
+        s.push(8);
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        /*
+
+        1. New stack s created.
+        2. add 7 and 10, now stack is [10, 7]
+        3. Print out s.pop, remove 10 and print it out
+        4. Print out s.peek, we do not remove 7 we just look at it
+        4. add 3 then 5 to stack. now stack is [5, 3, 7]
+        5. s.pop and remove value 5. Now stack is [3, 7], also print it out
+        6. s.Isempty returns false, print that out
+        7. check the size it should be 2, print it out
+        8. peek into next value on stack, its 3
+        9. add 8 to the stack now stack is [8, 3 ,7]
+        10. pop and print out value, value is 8 so now stack is [3, 7], print 8
+        11. pop and print out value, value is 3 so now stack is [7], print 3
 
         // 22. Write a piece of code that prints the elements of a queue of integers,
         // one per line.
@@ -52,14 +114,60 @@ class Lecture12 {
         // contents as it had at the start. In other words, don’t destroy the queue as
         // you print it. If you like, put your code into a method called print that
         // accepts the queue as a parameter.
-
+        */
+        Stack ques22Stack = new Stack<Integer>();
+        ques22Stack.push(5);
+        ques22Stack.push(10);
+        ques22Stack.push(15);
+        ques22Stack.push(20);
+        printStack(ques22Stack);
+        System.out.println(ques22Stack);
+        /*
         // 23. Write a piece of code that finds and prints the longest string in a stack
         // of strings.
-
+        */
+        Stack<String> ques22Stack2 = new Stack<String>();
+        ques22Stack2.push("hello");
+        ques22Stack2.push("hi");
+        ques22Stack2.push("goodbye");
+        ques22Stack2.push("howdy");
+        /*
         // For example, in the stack [hello, hi, goodbye, howdy], the longest string is
         // "goodbye". When your code is done running, the stack should still contain the
         // same contents as it had at the start. In other words, if you destroy the
         // stack as you examine it, restore its state afterward. If you like, put your
         // code into a method called printLongest that accepts the stack as a parameter.
+        */
+    }
+
+    public static void printLongest(Stack<Integer> s){
+        Stack ques22TempStack = new Stack<Integer>();
+        String str = "";
+        while(!s.isEmpty()) {
+            ques22TempStack.push(s.peek());
+            String strTemp = (String) ques22TempStack.peek();
+
+            if (strTemp.length() > str.length()){
+                str = strTemp;
+            }
+
+            System.out.println(s.pop());
+        }
+
+        System.out.println(str);
+        while(!ques22TempStack.isEmpty()){
+            s.push(ques22TempStack.pop());
+        }
+    }
+
+    public static void printStack(Stack s){
+        Stack ques22TempStack = new Stack<Integer>();
+        while(!s.isEmpty()) {
+            ques22TempStack.push(s.peek());
+            System.out.println(s.pop());
+        }
+        while(!ques22TempStack.isEmpty()){
+            s.push(ques22TempStack.pop());
+        }
     }
 }
